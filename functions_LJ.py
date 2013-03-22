e82dffc3f180b5527c120df07a9aa4900d36dd14
Jacob Stevenson <js850@cam.ac.uk>
2013-03-22 14:25:22 +0000

    clean up lennard jones functions

--- functions_LJ.py
+++ functions_LJ.py
@@ -1,11 +1,47 @@
 """
 benchmarking Lennard-Jones clusters
 """
-from base_function import BaseFunction
-from base_function import LJ
+import numpy as np
 
-class LJ30(LJ):
-    target_E = -128.286571
-    natoms = 30
+from base_function import BaseFunction
+from pygmin.potentials import LJ
+
+class LennardJones(LJ):
+    """
+    The Lennard Jones potential
+    
+    a mathematically simple model that approximates the interaction between a 
+    pair of neutral atoms or molecules.    
+    http://en.wikipedia.org/wiki/Lennard-Jones_potential
+    
+    E = sum_ij V(r_ij)
+    
+    where r_ij is the cartesian distance between atom i and atom j, and the
+    pair potential has the form
+    
+    V(r) = 4 * eps * ( (sigma / r)**12 - (sigma / r)**6
+    
+    Notes
+    -----
+    the double loop over many atoms makes this *very* slow in Python.  If it
+    were in a compiled language it would be much faster.
+    """
     def get_random_configuration(self):
         return np.random.uniform(-1,1,[3*self.natoms]) * float(self.natoms)**(1./3) 
+
+
+class LJ38(LennardJones):
+    natoms = 38
+    target_E = -173.928427
+
+class LJ30(LennardJones):
+    natoms = 30
+    target_E = -128.286571
+
+class LJ20(LennardJones):
+    natoms = 20
+    target_E = -77.177043
+
+class LJ13(LennardJones):
+    natoms = 13
+    target_E = -44.326801
